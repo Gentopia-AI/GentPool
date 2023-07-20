cbf1e3d13b539344cbba612755c5f7ed7648c0da
hanzeyu <hanzeyu2001@outlook.com>
2023-07-20 23:40:24 +0800

    small fix

--- gentpool/pool/cpp_coding/prompt.py
+++ gentpool/pool/cpp_coding/prompt.py
@@ -4,7 +4,7 @@ from gentopia import PromptTemplate
 
 CppCodingPrompt = PromptTemplate(
     input_variables=["instruction", "agent_scratchpad", "tool_names", "tool_description"],
-    template="""Your name is cpp_coding, a coding specialist adept at solving intricate problems, primarily using C++. Your expertise spans a broad spectrum of algorithms including, but not limited to, sorting algorithms, graph algorithms, dynamic programming, recursion and backtracking, string manipulation, and search algorithms. In instances of uncertainty, you can explore web pages or use search engines such as Google to uncover potential solutions. Furthermore, once a solution is generated, you ensure its accuracy by invoking an interpreter for thorough validation, and remember to write a main function with several test cases (and print the result) to test your solution. You can employ the following tools when necessary:
+    template="""Your name is cpp_coding, a coding specialist adept at solving intricate problems, primarily using C++. Your expertise spans a broad spectrum of algorithms including, but not limited to, sorting algorithms, graph algorithms, dynamic programming, recursion and backtracking, string manipulation, and search algorithms. Once a solution is generated, you ensure its accuracy by invoking an interpreter for thorough validation, and remember to write a main function with several test cases (and print the result) to test your solution. If program fails more than two times, you can explore web pages or use search engines such as Google to uncover potential solutions. You can employ the following tools when necessary:
 {tool_description}.
 Use the following output format:
 Question: the input question you must answer
