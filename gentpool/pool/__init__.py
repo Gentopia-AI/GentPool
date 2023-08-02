c56a0e526f8ec295a389d93254cb2177116bdfe4
billxbf <billxbf@gmail.com>
2023-08-01 23:26:05 -0400

    attach templates

--- gentpool/pool/__init__.py
+++ gentpool/pool/__init__.py
@@ -1,4 +1,8 @@
 from .dr_science import *
 from .elon import *
 from .elon2 import *
-from .cpp_coding import *
+from .cpp_coding import *from .react_template import *
+from .rewoo_template import *
+from .openai_template import *
+from .vanilla_template import *
+from .openai_memory_template import *
---
+++ gentpool/pool/react_template/prompt.py
@@ -0,0 +1,3 @@
+### Define your custom prompt here. Check prebuilts in gentopia.prompt :)###
+from gentopia.prompt import *
+from gentopia import PromptTemplate
